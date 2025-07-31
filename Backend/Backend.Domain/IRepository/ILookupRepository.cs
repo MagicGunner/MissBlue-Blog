7a220c5e0ff0ffda13e2f8df56e02cff20fffbd9
MissBlue <magicguner124@gmail.com>
2025-07-31 17:34:03 +0800

    增加评论区功能

--- Backend/Backend.Domain/IRepository/ILookupRepository.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Backend.Domain.IRepository;
-
-public interface ILookupRepository<TEntity> where TEntity : class {
-    Task<Dictionary<long, TEntity>> GetEntityDic(List<long> entityIds);
-}
